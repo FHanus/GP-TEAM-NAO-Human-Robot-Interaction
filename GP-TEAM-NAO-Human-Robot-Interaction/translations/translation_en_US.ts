950853fc5800b35d8ad3551bdf2f329ace31fcd0
FHanus <filiphanus1@gmail.com>
2024-11-29 10:39:39 +0000

    29/11/2024 Initial test

---
+++ GP-TEAM-NAO-Human-Robot-Interaction/translations/translation_en_US.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Stick em up everybody, I am robbin this place</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Stick em up everybody, I am robbin this place</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>

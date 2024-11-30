3f4b47bef96a0153e00879c455d4ec726e3939e6
FHanus <filiphanus1@gmail.com>
2024-11-30 23:33:38 +0000

    30/11/2024 Basic layout and simple functionality done

--- GP-TEAM-NAO-Human-Robot-Interaction/translations/translation_en_US.ts
+++ GP-TEAM-NAO-Human-Robot-Interaction/translations/translation_en_US.ts
@@ -54,12 +54,152 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/State 1/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Greetings traveller, do you have an appointment?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Greetings traveller, do you have an appointment?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/State 1/Choice</name>
+        <message>
+            <source>no/no nao/disagree/I disagree</source>
+            <comment>SpeechReco negative</comment>
+            <translation type="vanished">no/no nao/disagree/I disagree</translation>
+        </message>
+        <message>
+            <source>yes/yes nao/agree/I agree/of course</source>
+            <comment>SpeechReco positive</comment>
+            <translation type="vanished">yes/yes nao/agree/I agree/of course</translation>
+        </message>
+        <message>
+            <source>help/help me/i don't know</source>
+            <comment>SpeechReco help</comment>
+            <translation type="vanished">help/help me/i don't know</translation>
+        </message>
+        <message>
+            <source>exit/stop/quit</source>
+            <comment>SpeechReco exit</comment>
+            <translation type="vanished">exit/stop/quit</translation>
+        </message>
+        <message>
+            <source>repeat/pardon/what/excuse me</source>
+            <comment>SpeechReco repeat</comment>
+            <translation type="vanished">repeat/pardon/what/excuse me</translation>
+        </message>
+        <message>
+            <source>I understood %s. Is that correct?</source>
+            <comment>TTS confirmation</comment>
+            <translation type="vanished">I understood %s. Is that correct?</translation>
+        </message>
+        <message>
+            <source>, / or </source>
+            <comment>TTS enumMarks</comment>
+            <translation type="vanished">, / or </translation>
+        </message>
+        <message>
+            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
+            <comment>TTS helpEnumChoices</comment>
+            <translation type="vanished">No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</translation>
+        </message>
+        <message>
+            <source> you can ask me: %s. </source>
+            <comment>TTS helpEnumDefault</comment>
+            <translation type="vanished"> you can ask me: %s. </translation>
+        </message>
+        <message>
+            <source>You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</source>
+            <comment>TTS helpTactile</comment>
+            <translation type="vanished">You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</translation>
+        </message>
+        <message>
+            <source>I did not understand. </source>
+            <comment>TTS notUnderstood</comment>
+            <translation type="vanished">I did not understand. </translation>
+        </message>
+        <message>
+            <source>No question has been defined, so I cannot repeat it. </source>
+            <comment>TTS noQuestion</comment>
+            <translation type="vanished">No question has been defined, so I cannot repeat it. </translation>
+        </message>
+        <message>
+            <source>There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </source>
+            <comment>TTS notUnderstoodAnims</comment>
+            <translation type="vanished">There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/State 2/Animated Say</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Entering State 2</source>
             <comment>Text</comment>
             <translation type="unfinished">Entering State 2</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/State 2/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Who do you have an apointment with?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Who do you have an apointment with?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/State 2/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>sorry, didn't catch that</source>
+            <comment>Text</comment>
+            <translation type="unfinished">sorry, didn't catch that</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/State 2/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>hell yeah I know that dude</source>
+            <comment>Text</comment>
+            <translation type="unfinished">hell yeah I know that dude</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/State 2/Animated Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>hell yeah I know that gal</source>
+            <comment>Text</comment>
+            <translation type="unfinished">hell yeah I know that gal</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/State 2/Animated Say (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>may I ask for your confirmation number?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">may I ask for your confirmation number?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/State 2/Animated Say (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>hell nah</source>
+            <comment>Text</comment>
+            <translation type="unfinished">hell nah</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/State 2/Animated Say (7)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>hell yeah</source>
+            <comment>Text</comment>
+            <translation type="unfinished">hell yeah</translation>
         </message>
     </context>
     <context>
@@ -72,6 +212,15 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/State 3/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Do you want to call a human? Nobody ain't coming</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Do you want to call a human? Nobody ain't coming</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/State 4/Animated Say</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
@@ -81,12 +230,39 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/State 4/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Email sent (not really)</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Email sent (not really)</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/State 4/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Please wait for my coworker to show up</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Please wait for my coworker to show up</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/State 5/Animated Say</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Entering State 5</source>
             <comment>Text</comment>
             <translation type="unfinished">Entering State 5</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/State 5/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>the dude finally showed up</source>
+            <comment>Text</comment>
+            <translation type="unfinished">the dude finally showed up</translation>
         </message>
     </context>
 </TS>

b228ba80c3576a395230475a804ad482495f60f8
Harry Williams <harry4.williams@live.uwe.ac.uk>
2024-12-04 21:31:09 +0000

    Added email project folder

---
+++ email/translations/translation_en_US.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Who are you here to see</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Who are you here to see</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Please say again. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Please say again. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Get Localized Text</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>
